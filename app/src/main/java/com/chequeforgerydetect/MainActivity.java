5891e7ed29e1383e67fb7526fa4aa3c78549bd51
Anupam Wadhwa <anupamwadhwa@outlook.com>
2018-11-11 05:13:20 +0530

    OpenCV Setup

--- app/src/main/java/com/chequeforgerydetect/MainActivity.java
+++ app/src/main/java/com/chequeforgerydetect/MainActivity.java
@@ -2,12 +2,19 @@ package com.chequeforgerydetect;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import org.opencv.android.OpenCVLoader;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.print("Hello");
+        if (OpenCVLoader.initDebug()) {
+            Toast.makeText(getApplicationContext(), "OpenCV Loaded", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "Fail to load OpenCV", Toast.LENGTH_SHORT).show();
+        }
     }
 }
